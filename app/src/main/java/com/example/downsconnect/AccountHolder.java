78b6ec54613ec650f01906df52aeedce3423077b
Jeffrey <duahjeffrey08@gmail.com>
2021-02-20 20:15:09 -0500

    Initial database and objects

---
+++ app/src/main/java/com/example/downsconnect/AccountHolder.java
@@ -0,0 +1,53 @@
+package com.example.downsconnect;
+
+public class AccountHolder {
+    private String firstName, lastName, username, password, phone;
+
+    AccountHolder(String firstName, String lastName, String username, String password, String phone){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/example/downsconnect/Gender.java
@@ -0,0 +1,5 @@
+package com.example.downsconnect;
+
+public enum Gender {
+    MALE, FEMALE;
+}
