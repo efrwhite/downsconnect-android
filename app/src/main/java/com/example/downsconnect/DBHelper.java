c2101ad3c680f560a2145a81973bdbd24f40ed01
Jeffrey <duahjeffrey08@gmail.com>
2021-03-01 19:47:42 -0500

    More database work and connection between login screens

--- app/src/main/java/com/example/downsconnect/DBHelper.java
+++ app/src/main/java/com/example/downsconnect/DBHelper.java
@@ -9,9 +9,12 @@ import androidx.annotation.Nullable;
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "downsconnect.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String[] TABLE_NAMES = {"AccountHolders", "Children"};
+    private static final String[] TABLE_NAMES = {"AccountHolders", "Children", "Feed", "Mood", "Sleep"};
     private static final String[] COLUMN_1 = {"FirstName", "LastName", "Username", "Password", "Phone"};
-    private static final String[] COLUMN_2 = {"FirstName", "LastName", "Gender", "Bloodtype", "DueDate", "Birthday"};
+    private static final String[] COLUMN_2 = {"ChildID", "FirstName", "LastName", "Age" , "Gender", "BloodType", "DueDate", "Birthday", "Allergies"};
+    private static final String[] COLUMN_3 = {"FeedID", "ChildID", "Amount", "Substance", "TimeConsumed", "Notes"};
+    private static final String[] COLUMN_4 = {"MoodID", "ChildID", "MoodType", "Time", "Notes"};
+    private static final String[] COLUMN_5 = {"SleepID", "ChildID", "StartTime", "EndTime", "SleepType" ,"Notes"};
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -19,7 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE AccountHolders(" +
-                "AccountHolderID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                //"AccountHolderID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
                 "FirstName TEXT, " +
                 "LastName TEXT, " +
                 "Username TEXT, " +
@@ -29,14 +32,53 @@ public class DBHelper extends SQLiteOpenHelper {
                 "ChildID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
                 "FirstName TEXT, " +
                 "LastName TEXT, " +
+                "Age INTEGER, " +
                 "Gender TEXT," +
                 "BloodType TEXT," +
                 "DueDate TEXT, " +
-                "Birthday TEXT);");
+                "Birthday TEXT, " +
+                "Notes TEXT);");
+        db.execSQL("CREATE TABLE Feed(" +
+                "FeedID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "ChildID INTEGER, " +
+                "Amount INTEGER, " +
+                "Substance TEXT, " +
+                "TimeConsumed TEXT, " +
+                "Notes TEXT);");
+        db.execSQL("CREATE TABLE Mood(" +
+                "MoodID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "ChildID INTEGER, " +
+                "MoodType TEXT, " +
+                "Time TEXT, " +
+                "Notes TEXT);");
+        db.execSQL("CREATE TABLE Sleep(" +
+                "SleepID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "ChildID INTEGER, " +
+                "StartTime TEXT," +
+                "EndTime TEXT, " +
+                "SleepType TEXT, " +
+                "Notes TEXT);");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion != newVersion){
+            db.execSQL("DROP TABLE IF EXISTS AccountHolders");
+            db.execSQL("DROP TABLE IF EXISTS Children");
+            db.execSQL("DROP TABLE IF EXISTS Feed");
+            db.execSQL("DROP TABLE IF EXISTS Mood");
+        }
+    }
 
+    public void deleteEntry(int id, String table){
+        String tableID = table.substring(0, table.length() - 1);
+        for(String name : TABLE_NAMES){
+            if(name.equals(table)){
+                String query = "DELETE FROM " + table + " WHERE " + tableID + "ID = " + id + ";";
+                SQLiteDatabase db = this.getWritableDatabase();
+                db.execSQL(query);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/downsconnect/SignUpActivity.java
+++ app/src/main/java/com/example/downsconnect/SignUpActivity.java
@@ -1,6 +1,9 @@
 package com.example.downsconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,24 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signUp = findViewById(R.id.signUpButton);
+        Button cancel = findViewById(R.id.cancelButton);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
