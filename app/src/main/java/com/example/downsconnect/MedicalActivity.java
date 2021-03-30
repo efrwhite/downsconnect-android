d212680e926880d9f1e15f6a100d668367716f7c
Kai Nguyen <52805377+rainsreign@users.noreply.github.com>
2021-03-30 13:33:59 -0400

    Update spinner entries

--- app/src/main/java/com/example/downsconnect/MedicalActivity.java
+++ app/src/main/java/com/example/downsconnect/MedicalActivity.java
@@ -1,16 +1,22 @@
 package com.example.downsconnect;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
-public class MedicalActivity extends AppCompatActivity {
+public class MedicalActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private EditText doctorDatePicker;
+    private boolean doctorDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,7 @@ public class MedicalActivity extends AppCompatActivity {
 
         final Button back = findViewById(R.id.backButton);
         TextView currentTime = findViewById(R.id.current_time_text);
+        doctorDatePicker = findViewById(R.id.doctorDatePicker);
 
         Calendar calendar = Calendar.getInstance();
         long time = calendar.getTimeInMillis();
@@ -39,5 +46,30 @@ public class MedicalActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        doctorDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doctorDate = true;
+                showDatePickerDialog();
+            }
+        });
+    }
+
+
+
+    private void showDatePickerDialog() {
+        @SuppressLint("ResourceType") DatePickerDialog datePickerDialog = new DatePickerDialog(this, 2,this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        if(doctorDate){
+            doctorDatePicker.setText(month + "/" + day + "/" + year);
+        }
     }
 }
+
