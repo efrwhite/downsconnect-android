f5abe21b447688537fbccbc14676991b6127e0b9
Jeffrey <duahjeffrey08@gmail.com>
2021-04-06 20:03:37 -0400

    Add name section to child profiles and add ability to save medical info

--- app/src/main/java/com/example/downsconnect/DetailedProfileActivity.java
+++ app/src/main/java/com/example/downsconnect/DetailedProfileActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,12 +17,19 @@ import java.util.Calendar;
 
 public class DetailedProfileActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private EditText birthdayPicker, dueDatePicker;
+    private TextView age;
     private boolean birthday, dueDate;
+    private Calendar birthdayDate, due_Date;
     private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_profile);
+
+        birthdayDate = Calendar.getInstance();
+        due_Date = Calendar.getInstance();
+        age = findViewById(R.id.ageText);
+
 
         back = findViewById(R.id.backButton);
         Spinner genderSpinner = findViewById(R.id.genderEditText);
@@ -69,9 +77,17 @@ public class DetailedProfileActivity extends AppCompatActivity implements DatePi
 
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
         if(birthday) {
+            birthdayDate.set(year, month, day);
+            Calendar currentDate = Calendar.getInstance();
+            long difference = currentDate.getTimeInMillis() - birthdayDate.getTimeInMillis();
+            long days = difference / (24 * 60 * 60 * 1000);
+            int months = (int) days/ 30;
+            int years = (int) days/365;
+            age.setText(years + " year(s), " + months + " month(s), " + days + " day(s)");
             birthdayPicker.setText(month + "/" + day + "/" + year);
         }
         if(dueDate){
+            due_Date.set(year, month, day);
             dueDatePicker.setText(month + "/" + day + "/" + year);
         }
     }
