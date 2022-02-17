f8c649ecc36f5fbc745ce3f08a9935b3403a748b
Jeffrey <duahjeffrey08@gmail.com>
2022-02-16 20:48:52 -0500

    implement updating medications

--- app/src/main/java/com/iso/downsconnect/MedicationsActivity.java
+++ app/src/main/java/com/iso/downsconnect/MedicationsActivity.java
@@ -75,35 +75,55 @@ public class MedicationsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //check if all fields are filled out
-                if(!name.getText().toString().equals("") && !dose.getText().toString().equals("") && !frequency.getText().toString().equals("")
-                    && !reason.getText().toString().equals("") && !doseUnits.getSelectedItem().toString().equals("Select")){
-                    //store info into medication object
-                    med.setName(name.getText().toString());
-                    med.setDose(Double.parseDouble(dose.getText().toString()));
-                    med.setFrequency(frequency.getText().toString());
-                    med.setReason(reason.getText().toString());
-                    med.setDoseUnits(doseUnits.getSelectedItem().toString());
-                    //call add medication method and get id of newly inserted row
-                    long id = dbHelper.addMedication(med);
+                if (add.getText().toString().equals("Add")) {
+                    if (!name.getText().toString().equals("") && !dose.getText().toString().equals("") && !frequency.getText().toString().equals("")
+                            && !reason.getText().toString().equals("") && !doseUnits.getSelectedItem().toString().equals("Select")) {
+                        //store info into medication object
+                        med.setName(name.getText().toString());
+                        med.setDose(Double.parseDouble(dose.getText().toString()));
+                        med.setFrequency(frequency.getText().toString());
+                        med.setReason(reason.getText().toString());
+                        med.setDoseUnits(doseUnits.getSelectedItem().toString());
+                        //call add medication method and get id of newly inserted row
+                        long id = dbHelper.addMedication(med);
 
-                    //populate entry object and add it to database
-                    entry.setEntryType("Medication");
-                    entry.setForeignID(id);
-                    entry.setEntryText("Saved " + med.getName() + " medication for " + dbHelper.getChildName(childID));
-                    entry.setEntryTime(Calendar.getInstance().getTimeInMillis());
-                    entry.setChildID(childID);
-                    dbHelper.addEntry(entry);
+                        //populate entry object and add it to database
+                        entry.setEntryType("Medication");
+                        entry.setForeignID(id);
+                        entry.setEntryText("Saved " + med.getName() + " medication for " + dbHelper.getChildName(childID));
+                        entry.setEntryTime(Calendar.getInstance().getTimeInMillis());
+                        entry.setChildID(childID);
+                        dbHelper.addEntry(entry);
 
-                    //navigate back to Home Screen and display success message
-                    Toast.makeText(getApplicationContext(), "Medication Information saved", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MedicationsActivity.this, ActivityContainer.class);
-                    startActivity(intent);
+                        //navigate back to Home Screen and display success message
+                        Toast.makeText(getApplicationContext(), "Medication Information saved", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MedicationsActivity.this, ActivityContainer.class);
+                        startActivity(intent);
+                    } else {
+                        AlertDialog a = new AlertDialog.Builder(add.getContext()).create();
+                        a.setTitle("Missing Information");
+                        a.setMessage("Please make sure you've filled out the necessary information");
+                        a.show();
+                    }
                 }
                 else{
-                    AlertDialog a = new AlertDialog.Builder(add.getContext()).create();
-                    a.setTitle("Missing Information");
-                    a.setMessage("Please make sure you've filled out the necessary information");
-                    a.show();
+                    if (!name.getText().toString().equals("") && !dose.getText().toString().equals("") && !frequency.getText().toString().equals("")
+                            && !reason.getText().toString().equals("") && !doseUnits.getSelectedItem().toString().equals("Select")) {
+                        med.setName(name.getText().toString());
+                        med.setDose(Double.parseDouble(dose.getText().toString()));
+                        med.setFrequency(frequency.getText().toString());
+                        med.setReason(reason.getText().toString());
+                        med.setDoseUnits(doseUnits.getSelectedItem().toString());
+                        dbHelper.updateMedication(med);
+                        Toast.makeText(getApplicationContext(), "Medication Information updated", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MedicationsActivity.this, ActivityContainer.class);
+                        startActivity(intent);
+                    } else {
+                        AlertDialog a = new AlertDialog.Builder(add.getContext()).create();
+                        a.setTitle("Missing Information");
+                        a.setMessage("Please make sure you've filled out the necessary information");
+                        a.show();
+                    }
                 }
             }
         });
