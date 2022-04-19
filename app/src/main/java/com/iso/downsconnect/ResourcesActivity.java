4c1e91b08f3736d938ea54ad944dec427e5d34ec
Jeffrey <duahjeffrey08@gmail.com>
2022-04-18 21:54:14 -0400

    Fix resources addition functionality

--- app/src/main/java/com/iso/downsconnect/ResourcesActivity.java
+++ app/src/main/java/com/iso/downsconnect/ResourcesActivity.java
@@ -1,5 +1,6 @@
 package com.iso.downsconnect;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -11,7 +12,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.iso.downsconnect.helpers.DBHelper;
@@ -95,7 +98,18 @@ public class ResourcesActivity extends AppCompatActivity {
 
         for (final Resource resource: resources) {
             LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            textParams.setMargins(0, 30, 0, 30);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 0, 0, 30);
+
+            textParams.setMargins(0, 30, 0, 50);
+            ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            // set margins for parameters
+            marginLayoutParams.setMargins(20, 0, 50,10);
+
+            final LinearLayout horizontalLayout = new LinearLayout(this);
+            horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+            horizontalLayout.setLayoutParams(marginLayoutParams);
+            horizontalLayout.setId(resource.getResourceID());
 
             TextView resName = new TextView(this);
             resName.setText(resource.getName());
@@ -103,7 +117,20 @@ public class ResourcesActivity extends AppCompatActivity {
             resName.setTextColor(Color.parseColor("#0645AD"));
             resName.setWidth(450);
             resName.setLayoutParams(textParams);
-            resName.setOnClickListener(new View.OnClickListener() {
+//            resName.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent intent = new Intent();
+//                    intent.setAction(Intent.ACTION_VIEW);
+//                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//                    intent.setData(Uri.parse(resource.getURL()));
+//                    startActivity(intent);
+//                }
+//            });
+
+            Button visit = new Button(getApplicationContext());
+            visit.setText("Visit");
+            visit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent();
@@ -113,7 +140,31 @@ public class ResourcesActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-            resourceLayout.addView(resName);
+            Button delete = new Button(getApplicationContext());
+            delete.setText("Delete");
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(ResourcesActivity.this)
+                            .setTitle("Delete Resource")
+                            .setMessage("Are you sure you want to delete this resource?")
+                            .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    helper.deleteEntry(resource.getResourceID(), "Resource");
+                                    resourceLayout.removeView(findViewById(resource.getResourceID()));
+                                    Toast.makeText(getApplicationContext(), "Deleted a resource", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .setNegativeButton("no", null).show();
+                }
+            });
+            visit.setWidth(1);
+            visit.setLayoutParams(layoutParams);
+            horizontalLayout.addView(resName);
+            horizontalLayout.addView(visit);
+            horizontalLayout.addView(delete);
+            resourceLayout.addView(horizontalLayout);
         }
     }
 }
